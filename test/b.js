20b4a68b4c4973a99eaa2dc077b2a98b594b45db
monai <monai@cure.lt>
2015-06-14 13:49:20 +0300

    wip

--- test/b.js
+++ test/b.js
@@ -1,11 +1,12 @@
 var sconsole = require('../');
 
 sconsole = sconsole({
-    'ident': 'zapzap'
+    'ident': ''
 });
 // sconsole.log(sconsole.priority.info, 'b.js');
 // sconsole.close();
 
 var console_ = sconsole.console;
 
-console_.dir(process);
+console_.dir({'ok': 'bo'}, { colors: true });
+console_.dir({'ok': 'bo'}, { colors: true });
